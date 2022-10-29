6b01c03d99d5b09b0eac5fe4ef6c847df7d1404e
lxu58 <leoxu3515@gmail.com>
2022-10-28 23:50:29 -0700

    1

---
+++ Assets/Scripts/Room Objects/KidBuildingBlox.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KidBuildingBlox : MonoBehaviour
+{
+    [SerializeField] private string prompt;
+    public string InteractionPrompt => prompt;
+
+    [SerializeField] private string text;
+    public string paragraphText => text;
+
+    public bool Interact(Interactor interactor)
+    {
+        Debug.Log(message: "Opening Kid Building Blox!");
+        return true;
+    }
+}
