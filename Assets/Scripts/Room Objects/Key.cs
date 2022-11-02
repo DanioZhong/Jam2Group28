e6ae64c2b28e6b8a9a139cabcefe8ad4429204ab
dinoplane <isacsac2017@gmail.com>
2022-11-01 23:04:07 -0700

    Room added

--- Assets/Scripts/Room Objects/Key.cs
+++ Assets/Scripts/Room Objects/Key.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class KidBuildingBlox: MonoBehaviour, IInteractable
+public class Key: MonoBehaviour, IInteractable
 {
     public string keyName;
     [SerializeField] private string prompt;
