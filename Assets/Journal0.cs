9367e8d18c505dd676dfba6c351db760e47a02dd
Daniel Zhong <32878167+DanielZhong@users.noreply.github.com>
2022-11-02 20:39:36 -0700

    fix bugs

--- Assets/Journal0.cs
+++ Assets/Journal0.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Journal0 : MonoBehaviour
+public class Journal0 : MonoBehaviour, IInteractable
 {
     [SerializeField] private string prompt;
     public string InteractionPrompt => prompt;
